2a67b9b822cce9791a4c7071b8c5c682bcae6219
ljq12697 <928623130@qq.com>
2026-02-28 09:54:43 +0800

    Fix ChorusPBGUI bug due to matplotlib version change

--- ChorusPBGUI.py
+++ ChorusPBGUI.py
@@ -603,7 +603,7 @@ def main():
     tb.show()
 
     span = SpanSelector(tb.widget.canvas.ax1, tb.oneselect, 'horizontal', useblit=True,
-                                 rectprops=dict(alpha=0.3, facecolor='grey'))
+                        props=dict(alpha=0.3, facecolor='grey'))
 
     sys.exit(app.exec_())
 
